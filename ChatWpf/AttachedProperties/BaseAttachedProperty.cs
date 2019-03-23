6aed7a50a902cd7befd026306d3722ed82b85cd2
Gyrbu Rodion <fpsoff@outlook.com>
2019-03-23 15:20:22 +0300

    attached properties
    Co-Authored-By: semenikhinadary <semenikhinadary@users.noreply.github.com>

---
+++ ChatWpf/AttachedProperties/BaseAttachedProperty.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace ChatWpf.AttachedProperties
+{
+    public abstract class BaseAttachedProperty<TParent, TProperty> where TParent : BaseAttachedProperty<TParent, TProperty>, new()
+    {
+        public event Action<DependencyObject, DependencyPropertyChangedEventArgs> ValueChanged = (sender, e) => { };
+
+        public static TParent Instance { get; private set; } = new TParent();
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.RegisterAttached("Value", typeof(TProperty), typeof(BaseAttachedProperty<TParent, TProperty>), new UIPropertyMetadata(new PropertyChangedCallback(OnValuePropertyChanged)));
+
+        private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            Instance.OnValueChanged(d, e);
+
+            Instance.ValueChanged(d, e);
+        }
+
+        public static TProperty GetValue(DependencyObject d) => (TProperty)d.GetValue(ValueProperty);
+
+        public static void SetValue(DependencyObject d, TProperty value) => d.SetValue(ValueProperty, value);
+
+        public virtual void OnValueChanged(DependencyObject sender, DependencyPropertyChangedEventArgs e) { }
+
+    }
+}
