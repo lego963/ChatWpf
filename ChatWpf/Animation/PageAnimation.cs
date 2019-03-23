bc56fad1c4b00a6f608080d886bc2892ffd2f270
Semenikhina Darya <semenihinadasha@mail.ru>
2019-03-23 15:43:13 +0300

    animations
    some page slide in/out animations and button color changes

---
+++ ChatWpf/Animation/PageAnimation.cs
@@ -0,0 +1,9 @@
+﻿namespace ChatWpf.Animation
+{
+    public enum PageAnimation
+    {
+        None = 0,
+        SlideAndFadeInFromRight = 1,
+        SlideAndFadeOutToLeft = 2,
+    }
+}
---
+++ ChatWpf/Pages/BasePage.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ChatWpf.Animation;
+
+namespace ChatWpf.Pages
+{
+    public class BasePage : Page
+    {
+        public PageAnimation PageLoadAnimation { get; set; } = PageAnimation.SlideAndFadeInFromRight;
+
+        public PageAnimation PageUnloadAnimation { get; set; } = PageAnimation.SlideAndFadeOutToLeft;
+
+        public float SlideSeconds { get; set; } = 0.8f;
+
+        public BasePage()
+        {
+            if (PageLoadAnimation != PageAnimation.None)
+                Visibility = Visibility.Collapsed;
+
+            Loaded += BasePage_Loaded;
+        }
+
+        private async void BasePage_Loaded(object sender, RoutedEventArgs e)
+        {
+            await AnimateIn();
+        }
+
+        public async Task AnimateIn()
+        {
+            if (PageLoadAnimation == PageAnimation.None)
+                return;
+
+            switch (PageLoadAnimation)
+            {
+                case PageAnimation.SlideAndFadeInFromRight:
+                    await this.SlideAndFadeInFromRight(SlideSeconds);
+                    break;
+            }
+        }
+
+        public async Task AnimateOut()
+        {
+            if (PageUnloadAnimation == PageAnimation.None)
+                return;
+
+            switch (PageUnloadAnimation)
+            {
+                case PageAnimation.SlideAndFadeOutToLeft:
+                    await this.SlideAndFadeOutToLeft(SlideSeconds);
+                    break;
+            }
+        }
+    }
+}
